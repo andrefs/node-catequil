deca97d5919f52a0d9ed8ae09a2451c1a7803b4e
André Santos <andrefs@andrefs.com>
2016-04-20 23:31:13 +0100

    Adds connection to database

--- server/server.js
+++ server/server.js
@@ -1,15 +1,45 @@
-import  express from 'express';
+import express  from 'express';
+import mongoose from 'mongoose';
+import async    from 'async';
 let config = require('../config.'+(process.env.NODE_ENV || 'development'));
+
+// Database connection
+
+let mongo_uri = process.env.MONGO_URI;
+if(!mongo_uri && config && config.db && config.db.uri){
+    mongo_uri = config.db.uri;
+} else {
+    mongo_uri = 'mongodb://localhost/trycatch_chat_noconfig';
+    console.warn(`MongoDB URI not set, using hardcoded default: ${mongo_uri} ...`);
+}
+mongoose.connect(mongo_uri);
+
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+// ...
 let app = new express();
-
-const port = (config && config.server && config.server.port) ?
-        config.server.port : process.env.port || 9754;
-
 
 app.get('/status', function(req, res){
     res.send('Server is running and accepting requests!');
 });
 
-app.listen(port, function(){
-    console.log('Server running on http://localhost:'+port);
-});
+
+// Server port and start
+
+const port = process.env.PORT || (config && config.server && config.server.port) ? config.server.port : 9754;
+
+async.series([
+    function(next){ db.once('open', next); },
+    function(next){ 
+        console.info(`Connected to MongoDB database ${mongo_uri}`);
+        app.listen(port, next);
+    }],
+    function(err){
+        if(err){
+            consolle.error.bind(console, 'Error:');
+            process.exit(1);
+        }
+        console.info(`Server running on http://localhost:${port}`);
+    }
+);
