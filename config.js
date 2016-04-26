3cf5a0b6bd29deefb6480ea34dfe26ef5fd0f588
André Santos <andrefs@andrefs.com>
2016-04-26 23:43:12 +0100

    Moves config files to own folder

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 import fs from 'fs';
 
 try {
-    let file = './config.local.js';
+    let file = './config/config.local.js';
     fs.accessSync(file);
     console.info(`Loading config info from ${file}`);
     module.exports = require(file);
 } catch(er){
     if(process.env.NODE_ENV === 'production'){
-        let file = './config.production.js';
+        let file = './config/config.production.js';
         console.info(`Loading config info from ${file}`);
         module.exports = require(file);
     } else {
-        let file = './config.development.js';
+        let file = './config/config.development.js';
         console.info(`Loading config info from ${file}`);
         module.exports = require(file);
     }
